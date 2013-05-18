d2f4af8b55b2200aa556f814d0261e623c3387f3
fabrizio <fcavaniglia@gmail.com>
2013-05-18 09:55:49 +0200

    fragments joy

--- EasyCalcetto2013/src/edu/easycalcetto/fragments/TestFragment.java
+++ EasyCalcetto2013/src/edu/easycalcetto/fragments/TestFragment.java
@@ -1,7 +1,20 @@
 package edu.easycalcetto.fragments;
 
-import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import edu.easycalcetto.R;
 
-public class TestFragment extends Fragment {
+public class TestFragment extends DialogFragment {
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View view = inflater.inflate(R.layout.fragment_test, container, false);
+		return view;
+	}
+	
 	
 }
