11e5fef409a3c64ce5535916af4e5efd8ae033ca
fabrizio <fcavaniglia@gmail.com>
2013-05-22 16:31:12 +0200

    Mod #2

--- EasyCalcetto2013/src/edu/easycalcetto/ServerUtilities.java
+++ EasyCalcetto2013/src/edu/easycalcetto/ServerUtilities.java
@@ -14,6 +14,7 @@ import java.util.Map.Entry;
 import java.util.Random;
 
 import android.content.Context;
+import android.os.AsyncTask;
 import android.util.Log;
 
 import com.google.android.gcm.GCMRegistrar;
@@ -117,57 +118,67 @@ public final class ServerUtilities {
 	 * 
 	 * @param endpoint
 	 *            POST address.
-	 * @param params
+	 * @param mParams
 	 *            request parameters.
 	 * 
 	 * @throws IOException
 	 *             propagated from POST.
 	 */
-	private static void post(String endpoint, Map<String, String> params)
+	private static void post(final String endpoint,final Map<String, String> mParams)
 			throws IOException {
-		URL url;
-		try {
-			url = new URL(endpoint);
-		} catch (MalformedURLException e) {
-			throw new IllegalArgumentException("invalid url: " + endpoint);
-		}
-		StringBuilder bodyBuilder = new StringBuilder();
-		Iterator<Entry<String, String>> iterator = params.entrySet().iterator();
-		// constructs the POST body using the parameters
-		while (iterator.hasNext()) {
-			Entry<String, String> param = iterator.next();
-			bodyBuilder.append(param.getKey()).append('=')
-					.append(param.getValue());
-			if (iterator.hasNext()) {
-				bodyBuilder.append('&');
+		new AsyncTask<Void, Void, Void>() {
+			@Override
+			protected Void doInBackground(Void... params) {
+				URL url;
+				try {
+					url = new URL(endpoint);
+				} catch (MalformedURLException e) {
+					throw new IllegalArgumentException("invalid url: " + endpoint);
+				}
+				StringBuilder bodyBuilder = new StringBuilder();
+				Iterator<Entry<String, String>> iterator = mParams.entrySet().iterator();
+				// constructs the POST body using the parameters
+				while (iterator.hasNext()) {
+					Entry<String, String> param = iterator.next();
+					bodyBuilder.append(param.getKey()).append('=')
+							.append(param.getValue());
+					if (iterator.hasNext()) {
+						bodyBuilder.append('&');
+					}
+				}
+				String body = bodyBuilder.toString();
+				Log.v(TAG, "Posting '" + body + "' to " + url);
+				byte[] bytes = body.getBytes();
+				HttpURLConnection conn = null;
+				try {
+					conn = (HttpURLConnection) url.openConnection();
+					conn.setDoOutput(true);
+					conn.setUseCaches(false);
+					conn.setFixedLengthStreamingMode(bytes.length);
+					conn.setRequestMethod("POST");
+					conn.setRequestProperty("Content-Type",
+							"application/x-www-form-urlencoded;charset=UTF-8");
+					// post the request
+					OutputStream out = conn.getOutputStream();
+					out.write(bytes);
+					out.close();
+					// handle the response
+					int status = conn.getResponseCode();
+					if (status != 200) {
+						throw new IOException("Post failed with error code " + status);
+					}
+				} catch (IOException e) {
+					Log.e(TAG, "an error occurred on post", e);
+				} finally {
+					if (conn != null) {
+						conn.disconnect();
+					}
+				}
+				return null;
 			}
-		}
-		String body = bodyBuilder.toString();
-		Log.v(TAG, "Posting '" + body + "' to " + url);
-		byte[] bytes = body.getBytes();
-		HttpURLConnection conn = null;
-		try {
-			conn = (HttpURLConnection) url.openConnection();
-			conn.setDoOutput(true);
-			conn.setUseCaches(false);
-			conn.setFixedLengthStreamingMode(bytes.length);
-			conn.setRequestMethod("POST");
-			conn.setRequestProperty("Content-Type",
-					"application/x-www-form-urlencoded;charset=UTF-8");
-			// post the request
-			OutputStream out = conn.getOutputStream();
-			out.write(bytes);
-			out.close();
-			// handle the response
-			int status = conn.getResponseCode();
-			if (status != 200) {
-				throw new IOException("Post failed with error code " + status);
-			}
-		} finally {
-			if (conn != null) {
-				conn.disconnect();
-			}
-		}
+			
+		}.execute();
+		
 	}
 
 	private class RegisterRunnable implements Runnable {
